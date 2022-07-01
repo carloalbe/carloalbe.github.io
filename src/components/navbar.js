a01a2353ae56dabad40730a8362eb6df99d22643
alberts96 <carloalbertocarrucciu@outlook.com>
2022-07-01 18:04:37 +0200

    build

---
+++ src/components/navbar.js
@@ -0,0 +1,42 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../img/foto cv croped.png"
+//import 'bootstrap/scss/bootstrap.scss';
+
+function MyNavbar(){
+
+    return(
+      <Navbar bg="dark" variant="dark" expand="md" style={{minHeight:"15vh", position:"sticked",width:"100%", backgroundColor:'black'}}>
+       <Container>
+
+         <Navbar.Brand  href="/" style={{fontSize:'2rem',  fontFamily:'"papyrus", serif'}} >
+         {' '}
+        <img
+          alt="io"
+          src={logo}
+          style={{marginLeft:'0.5rem', marginRight:'1rem'}}
+          width="50"
+          height="50"
+          className="d-inline-block "
+        />{' '}
+     C.A. Carrucciu
+      </Navbar.Brand>
+
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto" style={{fontSize:'1rem'}} >
+      <Nav.Link href="/work">Work</Nav.Link>
+      <Nav.Link href="/projects">Projects</Nav.Link>
+      <Nav.Link href="/education">Education</Nav.Link>
+      <Nav.Link href="/certification">Certifications</Nav.Link>
+
+      </Nav>
+      </Navbar.Collapse>
+     
+   </Container>
+      
+        </Navbar>
+
+    );
+}
+
+export default MyNavbar;
